dfe10e7279df4315a6483072045e78e250901933
t_shitianming <shitianming@fenrir-tec.com>
2020-02-27 16:17:43 +0800

    基本框架构建 databind 导入

--- app/src/main/java/com/example/beijifushengji/activity/BaseActivity.kt
+++ app/src/main/java/com/example/beijifushengji/activity/BaseActivity.kt
@@ -1,76 +1,30 @@
 package com.example.beijifushengji.activity
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-import android.os.Build
-import androidx.annotation.ColorLong
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
+abstract class BaseActivity <T:ViewDataBinding>: AppCompatActivity() {
 
-abstract class BaseActivity : AppCompatActivity() {
+    private lateinit var dataBinding:T
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        beforeInitView()
-
-        if (useTransStatusBar()) {
-            setStatusColor(0xffffffff)
+        if (getLayoutId() != 0){
+            dataBinding = DataBindingUtil.setContentView(this@BaseActivity, getLayoutId())
+            dataBinding.lifecycleOwner = this
         }
-
-        if (getLayoutId() != 0) {
-            setContentView(getLayoutId())
-        }
-
-        initView()
-
-        initEvent()
+        initView();
     }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    abstract fun beforeInitView()
-
-    abstract fun getLayoutId(): Int
 
     abstract fun initView()
 
-    abstract fun initEvent()
+    abstract fun getLayoutId(): Int
 
-    /**
-     * if no need to use transparent StatusBar override this method
-     */
-    protected open fun useTransStatusBar(): Boolean {
-        return true
-    }
-
-    protected open fun setStatusColor(@ColorLong long: Long) {
-        setTranslucent(long)
-    }
-
-    @SuppressLint("ObsoleteSdkInt")
-    private fun setTranslucent(@ColorLong long: Long) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window = window
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.decorView.systemUiVisibility =
-                SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or  SYSTEM_UI_FLAG_LAYOUT_STABLE
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = long.toInt()
-        }
+    override fun onDestroy() {
+        dataBinding.unbind()
+        super.onDestroy()
     }
 
 }
