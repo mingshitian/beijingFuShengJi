4ffa76e0560e333ab2b993295561397a0c4e6839
t_shitianming <shitianming@fenrir-tec.com>
2020-03-03 17:18:26 +0800

    viewmodel协程支持

--- app/src/main/java/com/example/beijifushengji/activity/BaseActivity.kt
+++ app/src/main/java/com/example/beijifushengji/activity/BaseActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
+import java.lang.reflect.ParameterizedType
+import java.lang.Class as Class
 
-abstract class BaseActivity <T:ViewDataBinding>: AppCompatActivity() {
+
+abstract class BaseActivity <T:ViewDataBinding,VM:BaseViewModel>(useDataBinding: Boolean = true) : AppCompatActivity() {
 
     private lateinit var dataBinding:T
 
+    private val _useBinding = useDataBinding
+
+    lateinit var mViewModel: VM
+
+    private lateinit var mViewModelClass: Class<VM>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (getLayoutId() != 0){
-            dataBinding = DataBindingUtil.setContentView(this@BaseActivity, getLayoutId())
-            dataBinding.lifecycleOwner = this
+        try {
+            mViewModelClass = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[1] as Class<VM>
+            mViewModel = ViewModelProvider(this).get(mViewModelClass)
+
+        } catch (e: Exception) {
+
         }
-        initView();
+        if (_useBinding) {
+            dataBinding =  DataBindingUtil.setContentView(this@BaseActivity, getLayoutId())
+            dataBinding.lifecycleOwner =this
+        } else setContentView(getLayoutId())
+
+        initView()
     }
 
     abstract fun initView()
