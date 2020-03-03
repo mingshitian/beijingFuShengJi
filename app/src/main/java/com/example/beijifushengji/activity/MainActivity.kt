9b72f3d5e10af7fb2da2868b4227684edd38dbf4
t_shitianming <shitianming@fenrir-tec.com>
2020-03-03 17:18:58 +0800

    viewmodel协程支持

--- app/src/main/java/com/example/beijifushengji/activity/MainActivity.kt
+++ app/src/main/java/com/example/beijifushengji/activity/MainActivity.kt
@@ -3,11 +3,12 @@ package com.example.beijifushengji.activity
 import com.example.beijifushengji.R
 import com.example.beijifushengji.databinding.ActivityMainBinding
 
-class MainActivity: BaseActivity<ActivityMainBinding>() {
-
+class MainActivity: BaseActivity<ActivityMainBinding,LoginViewModel>() {
 
     override fun getLayoutId(): Int = R.layout.activity_main
+
     override fun initView() {
 
     }
+
 }
